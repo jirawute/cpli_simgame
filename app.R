78889fb4d4528808db6fe61c8a2c5e542dfd2d1b
jirawute <yong@Yongs-MacBook-Pro.local>
2018-04-05 08:06:49 +0700

    Ready for demo 5 Apr

--- app.R
+++ app.R
@@ -15,16 +15,15 @@ ui <- fluidPage(
       
       # h2(textOutput("currentTime")),
       h3(textOutput("round")),
-      
+      # 
       sliderInput("e", "Epsilon",
                   min = 0, max = 1,
                   value = 0.2, step = 0.1,
                   animate = TRUE),
-      # Input: 
+      
       sliderInput("exp", "Experience",
                   min = 0, max = 1,
                   value = 0.5, step = 0.1),
-      # Input: Animation with custom interval 
       sliderInput("n", "Number of Customer:",
                   min = 0, max = 2000000,
                   value = 10, step = 10000,pre="#",sep=","),
@@ -53,6 +52,8 @@ ui <- fluidPage(
                   tabPanel("Output", tableOutput("myOut")),
                   tabPanel("Input", tableOutput("myIn")),
                   tabPanel("EXP", tableOutput("myExp")),
+                  tabPanel("Temp1", tableOutput("t1")),
+                  tabPanel("Temp2", tableOutput("t2")),
                   tabPanel("Assumption", tableOutput("customer_assumption")),
                   tabPanel("Mass", plotOutput("customer_mass")),
                   tabPanel("Premium", plotOutput("customer_premium"))
@@ -64,14 +65,16 @@ ui <- fluidPage(
 
 # Define server logic for random distribution app ----
 server <- function(input, output) {
-  library(shiny)
   library(googlesheets)
-  mass_sample <-160000
-  premium_sample <-40000
+  library(shiny)
+  mass_sample <-1600000
+  premium_sample <-400000
   count<-mass_sample+premium_sample
+  dimension <- c(count,4)
+  
   df <- data.frame( Name = c("Price" , "Packaging" , "MKT-T" , "MKT-O" , "Material"),
                     Mass = c(0.45 , 0.2 ,0.05, 0.1, 0.2),
-                    Premium = c( 0.1, 0.35, 0.1, 0.15, 0.3))
+                    Premium = c( 0.2, 0.25, 0.1, 0.15, 0.3))
   
   r<-0.2
   customer_mass<- cbind(runif(mass_sample, df$Mass[1]*(1-r), df$Mass[1]*(1+r)),
@@ -86,53 +89,56 @@ server <- function(input, output) {
                            runif(premium_sample, df$Premium[5]*(1-r), df$Premium[5]*(1+r)))
   customer<-rbind(customer_mass,customer_premium)
   
-  exp<-array(0,c(count,4))
-  filter<-array(0,c(count,4))
-  filter[1:160000,]<-1
   
-  v <- reactiveValues(data = NULL)
   
-  observeEvent(input$run, {
-    v$data <-loadData()
-    v$out <-cal(v$data)
-  })
-  
-  observeEvent(input$reset, {
-    v$data <- NULL
-    v$out <- NULL
-    exp<-NULL
-  })  
-  cal<- function(data){
-    player_in <- as.matrix(data[2:6,])
+  cal<- function(v){
+    v$year<-v$year+1
+    player_in <- as.matrix(v$data[2:6,])
+    stock <- as.numeric(v$data[7,])
+    expTY <- as.numeric(v$data[8,])#Experience This Year
     class(player_in)<-"numeric"
-    rel_exp <- as.numeric(data[7,])
-    for(i in 1:4){
-      if(data[1,i]=="Premium"){
-        filter[1:160000,i]<-0
-        filter[160001:count,i]<-1
+    
+    if(is.null(v$exp)){v$exp <- array(0,dimension)}
+    
+    filter<-array(0,dimension)
+    
+    for(i in 1:dimension[2]){
+      if(v$data[1,i]=="Mass"){
+        filter[1:(input$n*4/5),i]<-1
+      }else{
+        filter[(mass_sample+1):(mass_sample+input$n/5),i]<-1
       }
     }
     
-    output<-customer %*% player_in +exp+ rnorm(dim(exp)[1]*dim(exp)[2],0,input$e)
+    
+    output<-customer %*% player_in +v$exp+ rnorm(dimension[1]*dimension[2],0,input$e)+filter*10
+    max <- apply(output, 1, max)
+    output <- output - max+1
+    output[output<1]<-0
     output <-output*filter
-    max <- apply(output[, 1:4], 1, max)
-    output <- output - cbind(max,max,max,max)
-    output[output==0]<-1
-    output[output<0]<-0
-     # if(is.null(exp)){exp<-array(0,c(count,4))}
-     #  for(i in 1:4){
-     # exp[,i] <- exp[,i]+input$exp*output[,i]*rel_exp[i]
-     # }
-    colSums(output)*12
+    
+    min <- pmin(colSums(output)*12,stock)
+ 
+    v$out<-rbind(min,colSums(output),stock)
+    
+    temp<- output
+    for(i in 1:4){
+      temp[,i] <- output[,i]*expTY[i]
+    }
+    ret <- v$exp*output
+    ret[ret!=0]<-1
+    v$ret <- colSums(ret)/colSums(output)
+    v$exp<- v$exp+ temp*input$exp*0.1
+    
+    v$t1<-rbind(ret[1:10,],array("X",c(2,4)),ret[1600001:1600010,])
+    v$t2<-rbind(temp[1:10,],array("X",c(2,4)),temp[1600001:1600010,])
   }
   
   loadData <- function() {
-    # Grab the Google Sheet
     sheet <- gs_title("Simulation Game")
-    # Read the data
-    gs_read(sheet,ws="input",range="c9:f16")
+
+    gs_read(sheet,ws="input",range="c9:f17")
   }
-  
   sliderValues <- reactive({
     
     data.frame(
@@ -140,29 +146,48 @@ server <- function(input, output) {
         "E",
         "Experience",
         "No. of Customer"
-        ),
+      ),
       Value = as.character(c(
         input$e,
         input$exp,
         input$n
-        )),
+      )),
       stringsAsFactors = FALSE)
     
   })
   
-
   
+  values <- reactiveValues(data = NULL,out=NULL,exp=NULL,year=0)
+  
+  observeEvent(input$run, {
+    values$data <-loadData()
+    cal(values)
+    
+  })
+  
+  observeEvent(input$reset, {
+    values$data <- NULL
+    values$out <- NULL
+    values$exp <- NULL
+    values$year<-0
+  })  
   output$myIn <- renderTable({
-    if (is.null(v$data)) return()
-    v$data
+    if (is.null(values$data)) return("NULL")
+    values$data
   })
   output$myOut <- renderTable({
-    if (is.null(v$data)) return()
-    v$out
+    if (is.null(values$out)) return("NULL")
+    values$out
   })
   output$myExp <- renderTable({
-    if (is.null(exp)) return()
-    exp
+    if (is.null(values$exp)) return("NULL")
+    cbind(colSums(values$exp),values$ret)
+  })
+  output$t1 <- renderTable({
+     values$t1
+  })
+  output$t2 <- renderTable({
+    values$t2
   })
   # Generate a summary of the data ----
   output$customer_assumption <- renderTable({
@@ -176,12 +201,12 @@ server <- function(input, output) {
     boxplot(customer_premium)
   })
   round<-reactive({
-    paste("XXXX",as.numeric(input$run))
+    paste("Year",values$year)
   })
   output$round <- renderText({
     round()
   })
-
+  
 }
 
 # Create Shiny app ----
