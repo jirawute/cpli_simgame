d6b457fbe5d3cf53250864cc3b77d33c2f0c0768
jirawute <yong@Yongs-MacBook-Pro.local>
2018-04-07 09:31:57 +0700

    Write to googlesheet and add up to 7 players

--- app.R
+++ app.R
@@ -23,10 +23,10 @@ ui <- fluidPage(
       
       sliderInput("exp", "Experience",
                   min = 0, max = 1,
-                  value = 0.5, step = 0.1),
+                  value = 0.2, step = 0.1),
       sliderInput("n", "Number of Customer:",
                   min = 0, max = 2000000,
-                  value = 10, step = 10000,pre="#",sep=","),
+                  value = 1000000, step = 10000,pre="#",sep=","),
       
       
       
@@ -66,37 +66,45 @@ ui <- fluidPage(
 # Define server logic for random distribution app ----
 server <- function(input, output) {
   library(googlesheets)
+  suppressMessages(library(dplyr))
   library(shiny)
   mass_sample <-1600000
   premium_sample <-400000
   count<-mass_sample+premium_sample
-  dimension <- c(count,4)
-  
-  df <- data.frame( Name = c("Price" , "Packaging" , "MKT-T" , "MKT-O" , "Material"),
-                    Mass = c(0.45 , 0.2 ,0.05, 0.1, 0.2),
-                    Premium = c( 0.2, 0.25, 0.1, 0.15, 0.3))
-  
+  players <- 7
+  dimension <- c(count,players)
   r<-0.2
+  year<-0
+  
+  df <- data.frame( Name = c("Price" , "Packaging" , "MKT-T" , "MKT-O" ,"MKT-P", "Material"),
+                    Mass = c(0.45 , 0.2 ,0.01, 0.1,0.04, 0.2),
+                    Premium = c( 0.2, 0.25, 0.05, 0.15, 0.05,0.3))
+  
+  
   customer_mass<- cbind(runif(mass_sample, df$Mass[1]*(1-r), df$Mass[1]*(1+r)),
                         runif(mass_sample, df$Mass[2]*(1-r), df$Mass[2]*(1+r)),
                         runif(mass_sample, df$Mass[3]*(1-r), df$Mass[3]*(1+r)),
                         runif(mass_sample, df$Mass[4]*(1-r), df$Mass[4]*(1+r)),
-                        runif(mass_sample, df$Mass[5]*(1-r), df$Mass[5]*(1+r)))
+                        runif(mass_sample, df$Mass[5]*(1-r), df$Mass[5]*(1+r)),
+                        runif(mass_sample, df$Mass[6]*(1-r), df$Mass[6]*(1+r)))
   customer_premium<- cbind(runif(premium_sample, df$Premium[1]*(1-r), df$Premium[1]*(1+r)),
                            runif(premium_sample, df$Premium[2]*(1-r), df$Premium[2]*(1+r)),
                            runif(premium_sample, df$Premium[3]*(1-r), df$Premium[3]*(1+r)),
                            runif(premium_sample, df$Premium[4]*(1-r), df$Premium[4]*(1+r)),
-                           runif(premium_sample, df$Premium[5]*(1-r), df$Premium[5]*(1+r)))
+                           runif(premium_sample, df$Premium[5]*(1-r), df$Premium[5]*(1+r)),
+                           runif(premium_sample, df$Premium[6]*(1-r), df$Premium[6]*(1+r)))
   customer<-rbind(customer_mass,customer_premium)
   
+  colnames(customer_mass)<-df$Name
+  colnames(customer_premium)<-df$Name
   
   
   cal<- function(v){
-    v$year<-v$year+1
-    player_in <- as.matrix(v$data[2:6,])
-    stock <- as.numeric(v$data[7,])
-    expTY <- as.numeric(v$data[8,])#Experience This Year
-    class(player_in)<-"numeric"
+    
+    player_in <- as.matrix(v$data[2:7,])
+    class(player_in) <- "numeric"
+    stock <- as.numeric(v$data[8,])
+    expTY <- as.numeric(v$data[9,])#Experience This Year
     
     if(is.null(v$exp)){v$exp <- array(0,dimension)}
     
@@ -110,34 +118,43 @@ server <- function(input, output) {
       }
     }
     
-    
     output<-customer %*% player_in +v$exp+ rnorm(dimension[1]*dimension[2],0,input$e)+filter*10
     max <- apply(output, 1, max)
     output <- output - max+1
     output[output<1]<-0
     output <-output*filter
-    
+
     min <- pmin(colSums(output)*12,stock)
- 
+    
     v$out<-rbind(min,colSums(output),stock)
     
-    temp<- output
-    for(i in 1:4){
-      temp[,i] <- output[,i]*expTY[i]
-    }
-    ret <- v$exp*output
-    ret[ret!=0]<-1
-    v$ret <- colSums(ret)/colSums(output)
-    v$exp<- v$exp+ temp*input$exp*0.1
-    
-    v$t1<-rbind(ret[1:10,],array("X",c(2,4)),ret[1600001:1600010,])
-    v$t2<-rbind(temp[1:10,],array("X",c(2,4)),temp[1600001:1600010,])
-  }
+     temp<- output
+     for(i in 1:players){
+     temp[,i] <- output[,i]*expTY[i]
+     }
+     ret <- v$exp*output
+     ret[ret!=0]<-1
+     v$ret <- colSums(ret)/colSums(output)
+     v$exp<- v$exp+ temp*input$exp*0.1
+
+     v$t1<-rbind(output[1:10,],array("X",c(2,players)),output[(mass_sample+1):(mass_sample+10),])
+     v$t2<-rbind(customer[1:10,],array("X",c(2,6)),customer[(mass_sample+1):(mass_sample+10),])
+     colSums(output)
+      }
   
-  loadData <- function() {
+  readData <- function() {
     sheet <- gs_title("Simulation Game")
 
-    gs_read(sheet,ws="input",range="c9:f17")
+    data <- gs_read(sheet,ws="input",range="c9:i18")
+    # rownames(data)<-df$Name
+    
+    data
+    }
+  writeData <- function(data) {
+    sheet <- gs_title("Simulation Game")
+    
+    gs_edit_cells(sheet, ws = "input", anchor="c37",byrow=TRUE, input = data, trim = TRUE)
+
   }
   sliderValues <- reactive({
     
@@ -160,8 +177,14 @@ server <- function(input, output) {
   values <- reactiveValues(data = NULL,out=NULL,exp=NULL,year=0)
   
   observeEvent(input$run, {
-    values$data <-loadData()
-    cal(values)
+    
+    values$year<- "XXXXX"
+    values$data <-readData()
+    out<- cal(values)
+    writeData(out)
+    year<-year +1
+    values$year<-year
+    
     
   })
   
@@ -181,7 +204,7 @@ server <- function(input, output) {
   })
   output$myExp <- renderTable({
     if (is.null(values$exp)) return("NULL")
-    cbind(colSums(values$exp),values$ret)
+    rbind(colSums(values$exp),values$ret)
   })
   output$t1 <- renderTable({
      values$t1
@@ -195,10 +218,10 @@ server <- function(input, output) {
   })
   # Generate an HTML table view of the data ----
   output$customer_mass <- renderPlot({
-    boxplot(customer_mass)
+    boxplot(customer_mass,main="Mass Customer",ylab="Decision Weighted", ylim=c(0,0.5))
   })
   output$customer_premium <- renderPlot({
-    boxplot(customer_premium)
+    boxplot(customer_premium,main="Premium Customer",ylab="Decision Weighted", ylim=c(0,0.5))
   })
   round<-reactive({
     paste("Year",values$year)
